fd01a89253dda03c6cd3f7a5cf185f652780330e
M8yki <M8yki@192.168.1.24>
2019-02-25 14:21:06 -0800

    Implement minimax non-parallel

--- src/chess/bots/SimpleSearcher.java
+++ src/chess/bots/SimpleSearcher.java
@@ -1,10 +1,11 @@
 package chess.bots;
 
+import java.util.List;
+
 import cse332.chess.interfaces.AbstractSearcher;
 import cse332.chess.interfaces.Board;
 import cse332.chess.interfaces.Evaluator;
 import cse332.chess.interfaces.Move;
-import cse332.exceptions.NotYetImplementedException;
 
 /**
  * This class should implement the minimax algorithm as described in the
@@ -19,7 +20,38 @@ public class SimpleSearcher<M extends Move<M>, B extends Board<M, B>> extends
         return best.move;
     }
 
-    static <M extends Move<M>, B extends Board<M, B>> BestMove<M> minimax(Evaluator<B> evaluator, B board, int depth) {
-        throw new NotYetImplementedException();
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	static <M extends Move<M>, B extends Board<M, B>> BestMove<M> minimax(Evaluator<B> evaluator, B board, int depth) {
+        List<M> moves = board.generateMoves();
+        if (moves.isEmpty() ) {
+        	if (board.inCheck()) {
+        		return new BestMove(-evaluator.mate() - depth);
+        	} else {
+        		return new BestMove(-evaluator.stalemate());
+        	}
+        } else if (depth == 0) {
+        	return new BestMove(evaluator.eval(board));
+        }
+//        if (depth == 0) {
+//        	return new BestMove(evaluator.eval(board));
+//        } else if (moves.isEmpty()) {
+//        	if (board.inCheck()) {
+//        		return new BestMove(-evaluator.mate() - depth);
+//        	} else {
+//        		return new BestMove(-evaluator.stalemate());
+//        	}
+//        }
+        int bestValue = -evaluator.infty();
+        M bestMove = null;
+        for (M move : moves) {
+        	board.applyMove(move);
+        	int value = -minimax(evaluator, board, depth - 1).value;
+        	board.undoMove();
+        	if (value > bestValue) {
+        		bestValue = value;
+        		bestMove = move;
+        	}
+        }
+        return new BestMove(bestMove, bestValue);
     }
 }
