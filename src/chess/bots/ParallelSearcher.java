b1d3468187e4d25a94cf7b1ec9c4a52b30d6d1e2
M8yki <M8yki@192.168.1.24>
2019-02-25 16:42:24 -0800

    Tried implementing parallel, needs to resolve classcast exception line24

--- src/chess/bots/ParallelSearcher.java
+++ src/chess/bots/ParallelSearcher.java
@@ -1,14 +1,66 @@
 package chess.bots;
 
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
 import cse332.chess.interfaces.AbstractSearcher;
 import cse332.chess.interfaces.Board;
+import cse332.chess.interfaces.Evaluator;
 import cse332.chess.interfaces.Move;
-import cse332.exceptions.NotYetImplementedException;
 
 
 public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
         AbstractSearcher<M, B> {
     public M getBestMove(B board, int myTime, int opTime) {
-        throw new NotYetImplementedException();
+    	/* Calculate the best move */
+        BestMove<M> best = minimax(this.evaluator, board, ply);
+        return best.move;
+    }
+    
+	@SuppressWarnings("unchecked")
+	static <M extends Move<M>, B extends Board<M, B>> BestMove<M> minimax(Evaluator<B> evaluator, B board, int depth) {
+        List<M> moves = board.generateMoves();
+        M[] arr = (M[]) moves.toArray();
+        return searchBestMove(arr, board);
+    }
+    
+    private static final int DIVIDE_CUTOFF = 2; // Maybe = to depth?
+	@SuppressWarnings("serial")
+	private static class SearchTask<M extends Move<M>, B extends Board<M, B>> extends RecursiveTask<BestMove<M>> {
+    	int lo; int hi; M[] arr; B board; Evaluator<B> evaluator;
+    	
+    	SearchTask(M[] arr, int lo, int hi, B board) {
+    		this.arr = arr;
+    		this.lo = lo;
+    		this.hi = hi;
+    		this.board = board;
+    	}
+    	
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		protected BestMove<M> compute() {
+			if (hi - lo <= DIVIDE_CUTOFF) {
+				return SimpleSearcher.minimax(evaluator, board, hi - lo); // What is the depth?
+			}
+			
+			int mid = lo + (hi - lo) / 2;
+			SearchTask left = new SearchTask(arr, lo, mid, board);
+			SearchTask right = new SearchTask(arr, mid, hi, board);
+			left.fork();
+			BestMove<M> rightBest = right.compute();
+			BestMove<M> leftBest = (BestMove<M>) left.join();
+			if (rightBest.value > leftBest.value) {
+				return right.compute();
+			} else {
+				return (BestMove<M>) left.join();
+			}
+		}
+    }
+    
+    static final ForkJoinPool POOL = new ForkJoinPool();
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	public static <M extends Move<M>, B extends Board<M, B>> BestMove<M> searchBestMove(M[] arr, B board) {
+    	SearchTask task = new SearchTask(arr, 0, arr.length, board);
+    	return POOL.invoke(task);
     }
 }
