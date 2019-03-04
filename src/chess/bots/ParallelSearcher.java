2aa058ea281d1e2d7be77e7fb782ece99d564fc2
M8yki <M8yki@192.168.1.24>
2019-03-03 21:18:55 -0800

    Use list instead of array

--- src/chess/bots/ParallelSearcher.java
+++ src/chess/bots/ParallelSearcher.java
@@ -17,24 +17,19 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
         return minimax(this.evaluator, board, ply, super.cutoff).move;
     }
     
-	@SuppressWarnings("unchecked")
 	static <M extends Move<M>, B extends Board<M, B>> BestMove<M> minimax(Evaluator<B> evaluator, B board, int depth, int cutoff) {
         List<M> moves = board.generateMoves();
-        Move<M>[] arr = (Move<M>[]) new Move[moves.size()];
-        for (int i = 0; i < moves.size(); i++) {
-        	arr[i] = moves.get(i);
-        }
-        return searchBestMove(arr, board, depth, cutoff, evaluator);
+        return searchBestMove(moves, board, depth, cutoff, evaluator);
     }
     
     private static final int DIVIDE_CUTOFF = 2; // Maybe = to depth?
     private static final ForkJoinPool POOL = new ForkJoinPool();
 	@SuppressWarnings("serial")
 	private static class SearchTask<M extends Move<M>, B extends Board<M, B>> extends RecursiveTask<BestMove<M>> {
-    	int lo; int hi; Move<M>[] arr; B board; Evaluator<B> evaluator; int cutoff; int depth;
+    	int lo; int hi; List<M> moves; B board; Evaluator<B> evaluator; int cutoff; int depth;
     	
-    	SearchTask(M[] arr, int lo, int hi, B board, int depth, int cutoff, Evaluator<B> evaluator) {
-    		this.arr = arr;
+    	SearchTask(List<M> moves, int lo, int hi, B board, int depth, int cutoff, Evaluator<B> evaluator) {
+    		this.moves = moves;
     		this.lo = lo;
     		this.hi = hi;
     		this.board = board;    	
@@ -43,23 +38,18 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
     		this.cutoff = cutoff;
     	}
     	
-    	@SuppressWarnings("unchecked")
 		SearchTask(Move<M> move, B board, int depth, int cutoff, Evaluator<B> evaluator) {
     		B newBoard = board.copy();
     		newBoard.applyMove(move.copy());
     		List<M> newMoves = newBoard.generateMoves();
-    		Move<M>[] newArr = (Move<M>[]) new Move[newMoves.size()];
-            for (int i = 0; i < newMoves.size(); i++) {
-            	newArr[i] = newMoves.get(i);
-            }
-            arr = newArr;
+            moves = newMoves;
             this.depth = depth;
             this.board = newBoard;
             this.depth = depth;
             this.cutoff = cutoff;
             this.evaluator = evaluator;
             this.lo = 0;
-            this.hi = arr.length;
+            this.hi = moves.size();
     	}
     	
 		@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -69,7 +59,7 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
 			} else if (hi - lo <= DIVIDE_CUTOFF) {
 				SearchTask[] tasks = new SearchTask[hi - lo];
 				for (int i = 0; i < hi - lo; i++) {
-					tasks[i] = new SearchTask(arr[i].copy(), board.copy(), depth - 1, cutoff, evaluator);
+					tasks[i] = new SearchTask(moves.get(i), board.copy(), depth - 1, cutoff, evaluator);
 					tasks[i].fork();
 				}
 				for (int i = 0; i < tasks.length; i++) {
@@ -77,8 +67,8 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
 				}
 			}
 			int mid = lo + (hi - lo) / 2;
-			SearchTask left = new SearchTask(arr, lo, mid, board.copy(), depth, cutoff, evaluator);
-			SearchTask right = new SearchTask(arr, mid, hi, board.copy(), depth, cutoff, evaluator);
+			SearchTask left = new SearchTask(moves, lo, mid, board.copy(), depth, cutoff, evaluator);
+			SearchTask right = new SearchTask(moves, mid, hi, board.copy(), depth, cutoff, evaluator);
 			left.fork();
 			BestMove<M> rightBest = right.compute();
 			BestMove<M> leftBest = (BestMove<M>) left.join();
@@ -92,8 +82,8 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
     
     @SuppressWarnings({ "unchecked", "rawtypes" })
 	public static <M extends Move<M>, B extends Board<M, B>> BestMove<M> searchBestMove(
-			 Move<M>[] arr, B board, int depth, int cutoff, Evaluator<B> evaluator) {
-    	SearchTask task = new SearchTask(arr, 0, arr.length, board, depth, cutoff, evaluator);
+			 List<M> moves, B board, int depth, int cutoff, Evaluator<B> evaluator) {
+    	SearchTask task = new SearchTask(moves, 0, moves.size(), board, depth, cutoff, evaluator);
     	return (BestMove<M>) POOL.invoke(task);
     }
 }
