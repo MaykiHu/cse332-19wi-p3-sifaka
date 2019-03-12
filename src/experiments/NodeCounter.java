65ef094017dfb2498143544d8a28d124c782ebc3
M8yki <M8yki@192.168.1.24>
2019-03-12 11:03:31 -0700

    Try to see if can run in GCE and print

--- src/experiments/NodeCounter.java
+++ src/experiments/NodeCounter.java
@@ -27,15 +27,13 @@ public class NodeCounter extends SearcherTests {
 	
 	@Override
 	protected void run() {
-        SHOW_TESTS = true;
-        PRINT_TESTERR = true;
 
 	    ALLOWED_TIME = Integer.MAX_VALUE;
 	    //test("depth1", TestingInputs1.FENS_TO_TEST.length); // Figure out how to change this in our SearcherTests in experiments
         test("depth2", TestingInputs1.FENS_TO_TEST.length);
-        test("depth3", TestingInputs1.FENS_TO_TEST.length);
-        test("depth4", TestingInputs1.FENS_TO_TEST.length);
-        test("depth5Test1", TestingInputs1.FENS_TO_TEST.length);
+        //test("depth3", TestingInputs1.FENS_TO_TEST.length);
+        //test("depth4", TestingInputs1.FENS_TO_TEST.length);
+        //test("depth5Test1", TestingInputs1.FENS_TO_TEST.length);
         
 		finish();
 	} 
