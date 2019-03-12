2ec1f71da77e00a6779deb0577ccae42c80e5d16
M8yki <M8yki@192.168.1.24>
2019-03-11 20:42:18 -0700

    Set ply to 5 because 6 sometimes timed out.

--- src/chess/setup/Engine.java
+++ src/chess/setup/Engine.java
@@ -24,7 +24,7 @@ public class Engine {
 	    searcher = new JamboreeSearcher<ArrayMove,ArrayBoard>();
 	
 	public Engine(int time, int inc) {
-        searcher.setDepth(6); // Lucky 6 in Chinese culture
+        searcher.setDepth(5); // Lucky 6 in Chinese culture
         searcher.setCutoff(2);
 
         searcher.setEvaluator(new SimpleEvaluator());
