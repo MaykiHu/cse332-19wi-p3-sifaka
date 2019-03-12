f174664845fe6d98b8bebcc81ecde1a56fe667b8
M8yki <M8yki@192.168.1.24>
2019-03-12 12:43:31 -0700

    Trying file path

--- src/experiments/NodeCounter.java
+++ src/experiments/NodeCounter.java
@@ -30,7 +30,7 @@ public class NodeCounter {
         //JamboreeSearcher<ArrayMove, ArrayBoard> searcher = new JamboreeSearcher<>();
         
         for (int ply = 1; ply <= 5; ply++) { // Test each ply
-        	Scanner inputs = new Scanner(new File("cse332-p3/src/experiments/TestingInputs.txt"));
+        	Scanner inputs = new Scanner(new File("TestingInputs.txt"));
         	int numInputs = 0;
         	while (inputs.hasNextLine()) {
         		numInputs++;
