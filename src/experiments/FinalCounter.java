edeaff414c9bb7dcb2fb9d0f64e817a5d9d76756
M8yki <M8yki@192.168.1.24>
2019-03-12 21:18:48 -0700

    Final test for AlphaBeta

--- src/experiments/FinalCounter.java
+++ src/experiments/FinalCounter.java
@@ -26,8 +26,8 @@ public class FinalCounter {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws FileNotFoundException {
         //SimpleSearcher<ArrayMove, ArrayBoard> searcher = new SimpleSearcher<>();
-        ParallelSearcher<ArrayMove, ArrayBoard> searcher = new ParallelSearcher<>();
-        //AlphaBetaSearcher<ArrayMove, ArrayBoard> searcher = new AlphaBetaSearcher<>();
+        //ParallelSearcher<ArrayMove, ArrayBoard> searcher = new ParallelSearcher<>();
+        AlphaBetaSearcher<ArrayMove, ArrayBoard> searcher = new AlphaBetaSearcher<>();
         //JamboreeSearcher<ArrayMove, ArrayBoard> searcher = new JamboreeSearcher<>();
         
         Scanner boards = new Scanner(new File("BoardInputs.txt"));
